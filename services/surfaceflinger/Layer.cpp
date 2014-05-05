d1148d1ce637b3bed27980b998f82c140d8ffc0f
Sravan Kumar D.V.N <sravank1@codeaurora.org>
2014-05-05 22:41:16 +0530

    sf: Use DirtyRect to optimize Full GPU composition
    Use layer DirtyRect information in optimizing full GPU composition by
    using startTile and endTile GL calls.
    
    Change-Id: Iec0f9de2ce667438bbcc00d02e500055068df05a

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -1425,6 +1425,12 @@ Layer::LayerCleaner::~LayerCleaner() {
     mFlinger->onLayerDestroyed(mLayer);
 }
 
+#ifdef QCOM_BSP
+bool Layer::hasNewFrame() const {
+   return (mQueuedFrames > 0);
+}
+#endif
+
 // ---------------------------------------------------------------------------
 }; // namespace android
 
